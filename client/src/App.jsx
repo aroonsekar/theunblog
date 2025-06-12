0f7a5deaf396ad50e265fa5496730539b26cf155
Arun Sekar <arunsekar@Aruns-MacBook-Pro.local>
2025-06-12 12:24:24 +0530

    vite seetup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+      <h1 className="text-4xl font-bold text-center text-blue-500 ">Tailwind is Live 🚀</h1>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
